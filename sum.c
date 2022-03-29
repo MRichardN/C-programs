4842b8cf9ecd3e3ac7953104bd59ecef9a7006ec
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2022-03-29 14:40:29 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+// Program to add two numbers
+#include<stdio.h>
+int main ()
+{
+  float number1 = 10.3265;
+  float number2 = 200.215;
+  float sum;
+
+  sum = number1 + number2;
+  printf("The sum of two numbers is %f", sum);
+
+  return 0;
+}
+
