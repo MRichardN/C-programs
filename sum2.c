8320424c1c678aa7128921ad9f7c0345efeaf0cd
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2022-03-29 14:49:03 +0300

    Add files via upload

---
+++ sum2.c
@@ -0,0 +1,23 @@
+// program to find sum of 2 numbers from the user
+#include<stdio.h>
+int main ()
+{
+  int a;
+  int b;
+  int sum;
+  
+  printf("Enter the first number \n");
+  scanf("%d", &a);
+
+  printf("Enter the second number \n");
+  scanf("%d", &b);
+
+  sum = a + b ;
+  printf("The sum of a and b is %d",sum);
+
+  
+  
+
+
+  return 0;
+}
