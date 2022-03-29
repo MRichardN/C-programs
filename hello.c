1a649280c1004c71371cc5f9a4bf66e3769e05ca
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2022-03-29 14:48:33 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,19 @@
+// A progran to print Hello World (Single line comment)
+
+/*
+Title: Hello World
+Author:  Adams
+Date: 22/03/2022
+(Multiple line Commnent)
+*/
+
+#include<stdio.h>
+int main ()
+{
+printf("achamagee \n \a");
+printf("nemie  \n");
+printf("habari ya dunia \n ");
+printf("iyabu ..");
+
+  return 0;
+}
